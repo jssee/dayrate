7b90339940f038dd8b9d291f1b29d6c32750279d
Jesse Hoyos <jesse@mojotech.com>
2020-02-19 16:42:38 -0500

    feat: get the math working

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,61 +1,108 @@
 import * as React from "react";
 import { NextPage } from "next";
 
-/* import { targetSalary, workingDays } from "../utils/formulas"; */
+import { targetSalary, workingDays } from "../utils/formulas";
 
 const initialState = {
-  rate: 0,
   gross: 0,
+  bonus: 0.01,
+  benefits: 0.2,
   sickdays: 7,
   holidays: 10,
-  nonBillableTime: 0.2,
-  bonus: 0.01,
-  benefits: 0.2
+  nonBillableTime: 0.2
 };
 
-function reducer(state: any, event: any) {
-  switch (event.type) {
-    case "update-sickdays":
-      return { ...state, sickdays: event.payload };
-    case "update-holidays":
-      return { ...state, holidays: event.payload };
-    case "update-nonBillableTime":
-      return { ...state, nonBillableTime: event.payload };
-    case "update-bonus":
-      return { ...state, bonus: event.payload };
-    case "update-benefits":
-      return { ...state, benefits: event.payload };
-    case "update-gross":
-      return { ...state, gross: event.payload };
-    case "update-rate":
-      return { ...state, rate: event.payload };
-    default:
-      throw new Error();
-  }
+function reducer(state: any, { field, value }: any) {
+  return {
+    ...state,
+    [field]: value
+  };
 }
 
 export default function(): React.ReactElement<NextPage> {
   const [state, dispatch] = React.useReducer(reducer, initialState);
+  const { gross, sickdays, holidays, nonBillableTime, bonus, benefits } = state;
+
+  const handleChange = (e: any) => {
+    dispatch({
+      field: e.target.name,
+      value: parseFloat(e.target.value)
+    });
+  };
 
   return (
     <main>
-      <form>
-        <label htmlFor="desired-gross">
-          Enter your desired gross salary:
-          <input
-            id="desired-gross"
-            name="desired-gross"
-            type="number"
-            onChange={(e: any) =>
-              dispatch({ type: "update-gross", payload: e.value })
-            }
-          />
-        </label>
-        <input type="submit" value="Calculate" />
-      </form>
+      <label htmlFor="gross">
+        Enter your desired gross salary:
+        <input
+          id="gross"
+          name="gross"
+          type="number"
+          value={gross}
+          onChange={handleChange}
+        />
+      </label>
+      <label htmlFor="sickdays">
+        Sickdays:
+        <input
+          id="sickdays"
+          name="sickdays"
+          type="number"
+          value={sickdays}
+          onChange={handleChange}
+        />
+      </label>
+      <label htmlFor="holidays">
+        Holidays:
+        <input
+          id="holidays"
+          name="holidays"
+          type="number"
+          value={holidays}
+          onChange={handleChange}
+        />
+      </label>
+      <label htmlFor="billableDays">
+        Non-billable time:
+        <input
+          id="nonBillableTime"
+          name="nonBillableTime"
+          type="number"
+          value={nonBillableTime}
+          onChange={handleChange}
+        />
+      </label>
+      <label htmlFor="bonus">
+        Bonus amount:
+        <input
+          id="bonus"
+          name="bonus"
+          type="number"
+          value={bonus}
+          onChange={handleChange}
+        />
+      </label>
+      <label htmlFor="benefits">
+        Benefits Cost:
+        <input
+          id="benefits"
+          name="benefits"
+          type="number"
+          value={benefits}
+          onChange={handleChange}
+        />
+      </label>
+
       <h1>
-        Your day rate should be:
-        {state.rate}
+        Target Salary: {targetSalary(gross, bonus, benefits)}
+        <br />
+        Working days: {workingDays(holidays, sickdays, nonBillableTime)}
+        <br />
+        Day rate:
+        {Math.round(
+          targetSalary(gross, bonus, benefits) /
+            workingDays(holidays, sickdays, nonBillableTime)
+        )}
       </h1>
     </main>
   );
--- utils/formulas.ts
+++ utils/formulas.ts
@@ -2,31 +2,26 @@ export function getPercent(n: number): number {
   return n / 100;
 }
 
-export function workingDays(holidays: number): number {
+export function workingDays(
+  holidays: number,
+  sickdays: number,
+  nonBillableTime: number
+): number {
   const DAYS = 365;
   const WEEKENDS = 104;
-
-  // On average, workers in private industry received 7 days of sick leave per year at 1 year of service.
-  // source: https://www.bls.gov/opub/ted/2019/private-industry-workers-with-sick-leave-benefits-received-8-days-per-year-at-20-years-of-service.htm
-  const SICK_DAYS = 7;
-
-  // We estimate that 20% of time spent is doing non-billable work
-  const nonBillableTime = 0.2;
-
   // The amount of total days after weekends, estimated sick time, and holidays
-  const billableDays: number = DAYS - WEEKENDS - SICK_DAYS - holidays;
-
+  const BILLABLE_DAYS: number = DAYS - WEEKENDS - sickdays - holidays;
   // we we return a whole number for simplicities sake
-  return Math.round(billableDays - billableDays * nonBillableTime);
+  return Math.round(BILLABLE_DAYS - BILLABLE_DAYS * nonBillableTime);
 }
 
-export function targetSalary(gross: number): number {
+export function targetSalary(gross: number, bonus: number, benefits: number) {
   // The average annual bonus is about 1% of your salary
-  const BONUS_AMT: number = gross * 0.01;
-
+  const BONUS_AMT: number = gross * bonus;
   // The average cost to cover benefits that a typical employer would cover, like health insurance, is 20%
-  const BENNY_AMT: number = gross * 0.2;
+  const BENNY_AMT: number = gross * benefits;
 
-  return Math.round(BONUS_AMT + BENNY_AMT + gross);
+  const TOTAL_AMT: number = gross + BONUS_AMT + BENNY_AMT;
+
+  return Math.round(TOTAL_AMT);
 }
-
